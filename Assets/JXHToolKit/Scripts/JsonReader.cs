e314348ffb0ec9631dee48293d14928d76a7ea27
Jiangxh <919575700@qq.com>
2016-10-05 14:54:03 +0800

    LitJosn插件读取txt解析json数据；

---
+++ Assets/JXHToolKit/Scripts/JsonReader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+
+public class FamilyInfo {  
+	public string name;  
+	public int age;  
+	public string tellphone;  
+	public string address;  
+}  
+
+public class FamilyList {  
+	public List<FamilyInfo> family_list;  
+} 
+
+public class JsonReader : MonoBehaviour {
+
+	public FamilyList m_FamilyList = null;
+	public TextAsset JsonTxt;
+
+	// Use this for initialization
+	void Start () {
+		string josnStr = JsonTxt.text;
+		m_FamilyList = JsonMapper.ToObject<FamilyList> (josnStr);
+		foreach (FamilyInfo info in m_FamilyList.family_list) {  
+			Debug.Log("Name:" + info.name + "       Age:" + info.age + "        Tel:" + info.tellphone + "      Addr:" + info.address);  
+		} 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
